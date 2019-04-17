24136447ac30272fc1c990655cebeed90f19dee8
ybj <1793870688@qq.com>
2019-04-17 18:21:33 +0800

    css布局，非栅格布局， 基本功能都阔以， 能用

---
+++ BootstrapComponent/js/menu_min.js
@@ -0,0 +1,61 @@
+/****************************************************************
+ *                                                                *
+ *                              代码库                            *
+ *                        www.dmaku.com                            *
+ *              努力创建完善、持续更新插件以及模板            *
+ *                                                                *
+ ****************************************************************/
+(function ($) {
+    $.fn.menu = function (b) {
+        var c,
+            item,
+            httpAdress;
+        b = jQuery.extend({
+                Speed: 220,
+                autostart: 1,
+                autohide: 1
+            },
+            b);
+        c = $(this);
+        item = c.children("ul").parent("li").children("a");
+        httpAdress = window.location;
+        item.addClass("inactive");
+
+        function _item() {
+            var a = $(this);
+            if (b.autohide) {
+                a.parent().parent().find(".active").parent("li").children("ul").slideUp(b.Speed / 1.2,
+                    function () {
+                        $(this).parent("li").children("a").removeAttr("class");
+                        $(this).parent("li").children("a").attr("class", "inactive")
+                    })
+            }
+            if (a.attr("class") == "inactive") {
+                a.parent("li").children("ul").slideDown(b.Speed,
+                    function () {
+                        a.removeAttr("class");
+                        a.addClass("active")
+                    })
+            }
+            if (a.attr("class") == "active") {
+                a.removeAttr("class");
+                a.addClass("inactive");
+                a.parent("li").children("ul").slideUp(b.Speed)
+            }
+        }
+
+        item.unbind('click').click(_item);
+        if (b.autostart) {
+            c.children("a").each(function () {
+                if (this.href == httpAdress) {
+                    $(this).parent("li").parent("ul").slideDown(b.Speed,
+                        function () {
+                            $(this).parent("li").children(".inactive").removeAttr("class");
+                            $(this).parent("li").children("a").addClass("active")
+                        })
+                }
+            })
+        }
+    }
+})(jQuery);
+console.log("\u002f\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u000d\u000a\u0020\u002a\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u0009\u0009\u000d\u000a\u0020\u002a\u0020\u0009\u0009\u0009\u0009\u0009\u0009\u0020\u0020\u0020\u0020\u0020\u0020\u4ee3\u7801\u5e93\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u000d\u000a\u0020\u002a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0077\u0077\u0077\u002e\u0064\u006d\u0061\u006b\u0075\u002e\u0063\u006f\u006d\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u000d\u000a\u0020\u002a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0009\u0009\u0020\u0020\u52aa\u529b\u521b\u5efa\u5b8c\u5584\u3001\u6301\u7eed\u66f4\u65b0\u63d2\u4ef6\u4ee5\u53ca\u6a21\u677f\u0009\u0009\u0009\u002a\u000d\u000a\u0020\u002a\u0020\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u000d\u000a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002f");
